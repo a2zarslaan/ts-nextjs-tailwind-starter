5aea6ecb37ddd3fb35b4dd588cda44c8b6a03d77
Amar Arslaan <a2zarslaan@hotmail.com>
2022-01-02 23:03:06 +0000

    chore: add acceptable image domains to next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: [
+      'i.imgur.com',
+      'platform-lookaside.fbsbx.com',
+      'firebasestorage.googleapis.com',
+    ],
+  },
   eslint: {
     dirs: ['src'],
   },
