aaa44301c3d59145be1b4d9f91131bac95a4e3a8
Amar Arslaan <a2zarslaan@hotmail.com>
2022-01-03 20:08:14 +0000

    feat: ui for header right

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import {
   FlagIcon,
-  HomeIcon,
   PlayIcon,
   SearchIcon,
-  ShoppingCartIcon,
-  UserGroupIcon} from "@heroicons/react/outline"
+  ShoppingCartIcon, 
+  ViewGridIcon} from "@heroicons/react/outline"
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,  HomeIcon,
+  UserGroupIcon,
+ } from "@heroicons/react/solid"
 import Image from "next/image";
 import React from 'react';
 
@@ -13,6 +18,7 @@ import HeaderIcon from '@/components/HeaderIcon';
 function Header() {
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/* left */}
       <div className="flex items-center">
         <Image 
           src="https://i.imgur.com/VgkNYXI.png"
@@ -22,14 +28,14 @@ function Header() {
         />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600"/>
-          <input className="flex ml-2 items-center bg-transparent outline-none border-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
+          <input className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none border-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
         </div>
       </div>
 
       {/* center */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
-          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon active Icon={HomeIcon} />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
           <HeaderIcon Icon={ShoppingCartIcon} />
@@ -38,6 +44,16 @@ function Header() {
       </div>
 
       {/* right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* profile pic */}
+
+        <p className="whitespace-nowrap font-semibold pr-3">Amar Arslaan</p>
+          <ViewGridIcon className="icon" />
+          <ChatIcon className="icon" />
+          <BellIcon className="icon" />
+          <ChevronDownIcon className="icon" />
+      </div>
+
     </div>
   )
 }
